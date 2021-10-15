3f97e57fa37c7bd237fdcb2ee73c59f2ccaaac6a
user <user@DESKTOP-9GQIHDR>
2021-10-15 17:05:56 +0900

    AdminOrderVO수정

--- springProject/src/main/java/kr/spring/adminOrder/vo/AdminOrderVO.java
+++ springProject/src/main/java/kr/spring/adminOrder/vo/AdminOrderVO.java
@@ -11,11 +11,18 @@ public class AdminOrderVO {
 	private int order_method;
 	private Date order_date;
 	private int order_pay;
+	private int delivery_pay;
 	private String mem_id; //JOIN - member
 	private String mem_name; //JOIN - member_detail
-	private String mem_phone; //JOIN - member_detail
 	private String d_status_name; //JOIN - delivery_status
 
+	
+	public int getDelivery_pay() {
+		return delivery_pay;
+	}
+	public void setDelivery_pay(int delivery_pay) {
+		this.delivery_pay = delivery_pay;
+	}
 	public int getOrder_no() {
 		return order_no;
 	}
@@ -76,24 +83,24 @@ public class AdminOrderVO {
 	public void setMem_name(String mem_name) {
 		this.mem_name = mem_name;
 	}
-	public String getMem_phone() {
-		return mem_phone;
-	}
-	public void setMem_phone(String mem_phone) {
-		this.mem_phone = mem_phone;
-	}
 	public String getD_status_name() {
 		return d_status_name;
 	}
 	public void setD_status_name(String d_status_name) {
 		this.d_status_name = d_status_name;
 	}
-
 	@Override
 	public String toString() {
 		return "AdminOrderVO [order_no=" + order_no + ", mem_num=" + mem_num + ", order_zipcode=" + order_zipcode
 				+ ", order_address1=" + order_address1 + ", order_address2=" + order_address2 + ", order_method="
-				+ order_method + ", order_date=" + order_date + ", order_pay=" + order_pay + ", mem_id=" + mem_id
-				+ ", mem_name=" + mem_name + ", mem_phone=" + mem_phone + ", d_status_name=" + d_status_name + "]";
+				+ order_method + ", order_date=" + order_date + ", order_pay=" + order_pay + ", delivery_pay="
+				+ delivery_pay + ", mem_id=" + mem_id + ", mem_name=" + mem_name + ", d_status_name=" + d_status_name
+				+ ", getDelivery_pay()=" + getDelivery_pay() + ", getOrder_no()=" + getOrder_no() + ", getMem_num()="
+				+ getMem_num() + ", getOrder_zipcode()=" + getOrder_zipcode() + ", getOrder_address1()="
+				+ getOrder_address1() + ", getOrder_address2()=" + getOrder_address2() + ", getOrder_method()="
+				+ getOrder_method() + ", getOrder_date()=" + getOrder_date() + ", getOrder_pay()=" + getOrder_pay()
+				+ ", getMem_id()=" + getMem_id() + ", getMem_name()=" + getMem_name() + ", getD_status_name()="
+				+ getD_status_name() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()="
+				+ super.toString() + "]";
 	}
 }
